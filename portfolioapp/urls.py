7a8039431bc09690702d1c76e83a04aac1bee03f
Raju-Mia <rajucse00@gmail.com>
2022-11-03 00:31:51 +0600

    wrok on data query by model, and make some pages.

--- portfolioapp/urls.py
+++ portfolioapp/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path('check/', views.check, name='check'),
     path('<name>/about-me/', views.about_me, name='about-me'), #dynamic url pass for get data.
+    path('<name>/skill/', views.skill, name='skill'),
+    path('<name>/intarest/', views.intarest, name='intarest'),
+    path('<name>/award/', views.award, name='award'),
+    path('<name>/education/', views.education, name='education'),
+    path('<name>/exprience/', views.exprience, name='exprience'),
 
 ]
--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -11,6 +11,33 @@ def check(request):
 
 
 def about_me(request,name):
-    about_me = AboutMe.objects.get(user__username = name)
-    context = {'about_me':about_me}
+    about_mee = AboutMe.objects.get(user__username = name)
+    context = {'about_m':about_mee}
     return render(request, 'about_me.html', context)
+
+
+def skill(request,name):
+    skillss = Skill.objects.filter(user__username = name)
+    context = {'skills': skillss}
+    return render(request, 'skill.html', context)
+
+def intarest(request,name):
+    intarestes = Interest.objects.filter(user__username = name)
+    context = {'intarests': intarestes}
+    return render(request, 'intarest.html', context)
+
+
+
+def award(request,name):
+    awardes = Award.objects.filter(user__username = name)
+    context = {'awards': awardes}
+    return render(request, 'award.html', context)
+
+
+def education(request,name):
+    pass
+
+
+
+def exprience(request,name):
+    pass
