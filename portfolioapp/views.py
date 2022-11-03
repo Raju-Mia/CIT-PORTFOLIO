4b449e800cbfb793f1ec7217c9f7a4735e59e9d1
Raju-Mia <rajucse00@gmail.com>
2022-11-03 20:23:52 +0600

    work on templates, data query and dainamic username pass in template wtih html

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -21,30 +21,30 @@ def about_me(request,name):
 
 def skill(request,name):
     skillss = Skill.objects.filter(user__username = name)
-    context = {'skills': skillss}
+    context = {'skills': skillss, 'name':name } #name is user_name 
     return render(request, 'skill.html', context)
 
 def intarest(request,name):
     intarestes = Interest.objects.filter(user__username = name)
-    context = {'intarests': intarestes}
+    context = {'intarests': intarestes, 'name':name}
     return render(request, 'intarest.html', context)
 
 
 
 def award(request,name):
     awardes = Award.objects.filter(user__username = name)
-    context = {'awards': awardes}
+    context = {'awards': awardes, 'name':name}
     return render(request, 'award.html', context)
 
 
 def education(request,name):
     educations = Education.objects.filter(user__username = name)
-    context = {'education':educations}
+    context = {'education':educations, 'name':name}
     return render(request, 'education.html', context)
 
 
 
 def exprience(request,name):
     expriences = Experience.objects.filter(user__username = name)
-    context = {'exprience':expriences}
+    context = {'exprience':expriences, 'name':name}
     return render(request, 'exprience.html', context)
